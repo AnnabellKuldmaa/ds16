ca636f026b36002352d27f7f8d749d7960d384b8
Markus Lippus <markus.lippus@stacc.ee>
2016-11-15 16:16:18 +0200

    updated server

--- src/server.py
+++ src/server.py
@@ -3,26 +3,24 @@ from threading import Thread, Lock
 
 import responses as rsp
 
-
 file_dict = {}
 user_dict = {}
+online_clients = set()
 
 
 def handle_client(sock):
-    user_name = sock.recv(rsp.BUFFER_SIZE)
-    print(user_name)
-    if user_name:
-        if user_name in user_dict:  # return file list if username exists
-            response = rsp.MSG_SEP.join([rsp.__FILE_LIST] + user_dict[user_name])
-        else:  # else add user to dict
-            user_dict[user_name] = []
-            response = rsp.__NEW_USER
-        sock.send(response)
-    else:  # return if empty string sent
-        return
-
     while True:
         message = sock.recv(rsp.BUFFER_SIZE)
+        message = message.split(rsp.MSG_SEP)
+        req_code = message[0]
+
+        if len(message) > 1:
+            action = command_dict[message[0]]  # Get action from dict
+            response = action(message[1])
+        else:
+            response = command_dict[message]
+
+        sock.send(response)
 
 
 if __name__ == '__main__':
@@ -45,3 +43,20 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         print 'Terminating ...'
         s.close()
+
+
+def request_user(user_name):
+    if user_name in user_dict:  # return file list if username exists
+        response = rsp.MSG_SEP.join([rsp.__FILES] + user_dict[user_name])
+    else:  # else add user to dict
+        user_dict[user_name] = []
+        response = rsp.__RESP_OK
+    return response
+
+def edit_file(filename):
+    with open('filename'):
+
+
+
+command_dict = {rsp.__GET_FILES: request_user,
+                rsp.__EDIT_FILE: edit_file}
