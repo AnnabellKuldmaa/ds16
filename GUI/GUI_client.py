d846acb2c72bc9c148cdc9724167ecaf7ada69ca
Heikki Saul <heikki.saul@gmail.com>
2016-11-15 17:10:01 +0200

    created GUI code

---
+++ GUI/GUI_client.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'GUI_client.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(801, 597)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.main_text_edit = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.main_text_edit.setGeometry(QtCore.QRect(20, 50, 761, 501))
+        self.main_text_edit.setObjectName("main_text_edit")
+        self.connect_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.connect_btn.setGeometry(QtCore.QRect(20, 10, 75, 23))
+        self.connect_btn.setObjectName("connect_btn")
+        self.newfile_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.newfile_btn.setGeometry(QtCore.QRect(340, 10, 75, 23))
+        self.newfile_btn.setObjectName("newfile_btn")
+        self.perm_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.perm_btn.setGeometry(QtCore.QRect(580, 10, 75, 23))
+        self.perm_btn.setObjectName("perm_btn")
+        self.IP_edit = QtWidgets.QLineEdit(self.centralwidget)
+        self.IP_edit.setGeometry(QtCore.QRect(100, 10, 113, 20))
+        self.IP_edit.setInputMask("")
+        self.IP_edit.setText("")
+        self.IP_edit.setObjectName("IP_edit")
+        self.user_edit = QtWidgets.QLineEdit(self.centralwidget)
+        self.user_edit.setGeometry(QtCore.QRect(220, 10, 113, 20))
+        self.user_edit.setObjectName("user_edit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(500, 10, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(420, 10, 69, 22))
+        self.comboBox.setObjectName("comboBox")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.connect_btn.setText(_translate("MainWindow", "Connect"))
+        self.newfile_btn.setText(_translate("MainWindow", "New File"))
+        self.perm_btn.setText(_translate("MainWindow", "Permissions"))
+        self.IP_edit.setPlaceholderText(_translate("MainWindow", "IP address:port"))
+        self.user_edit.setPlaceholderText(_translate("MainWindow", "Username"))
+        self.pushButton.setText(_translate("MainWindow", "Edit File"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
