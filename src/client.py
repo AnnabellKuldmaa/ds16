6da77eec64a4d4b0ae2f876f0e6a0a38b000c444
Markus Lippus <markus.lippus@stacc.ee>
2016-11-23 20:25:59 +0200

    Fixed merge issues

--- src/client.py
+++ src/client.py
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
-from socket import AF_INET, SOCK_STREAM, socket
-import sys
-import os
-WORK_DIR = os.getcwd()
-#print(WORK_DIR)
-sys.path.append(os.path.join(WORK_DIR, 'GUI'))
-#sys.path.append('../GUI')
-import responses as rsp
-import txteditor
-from threading import Thread
-#from multiprocessing import Queue
-from Queue import Queue
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtCore import QThread
-import time
-=======
+
 import responses as rsp
 from PyQt5.QtCore import QThread, pyqtSignal
->>>>>>> paleodieet
 import traceback
 
 
@@ -54,11 +37,7 @@ class Client(QThread):
         except Exception:
             traceback.print_exc()
             self._s.close()
-<<<<<<< HEAD
             print ('Ctrl+C issued, terminating ...')
-=======
-            print ('Ctrl+C issued, terminating ...' )
->>>>>>> paleodieet
             m = ''
         return m
 
@@ -79,19 +58,11 @@ class Client(QThread):
             self.new_filename.emit(msg_content)
         elif req_code in [rsp._UPDATE_FILE, rsp._FILE_CONTENT]:
             print('client received', msg_content)
-<<<<<<< HEAD
-            # TODO> SEE TEXTBOX OLEMA DISABLED
-            self._io.write_text(msg_content[0]) 
-
-        # self._s.send('asdasd')
-        # if req_code == 
-=======
             self.new_text.emit(msg_content[0])
         elif req_code == rsp._FILE_LIST:
             self.new_filelist.emit(msg_content)
         elif req_code == rsp._PERM_LIST:
             self.new_perm.emit(','.join(msg_content))
->>>>>>> paleodieet
         print ('processing message')
         # return
     
@@ -111,44 +82,4 @@ class Client(QThread):
         self._s.close()
 
     def run(self):
-<<<<<<< HEAD
-        while True:
-            try:
-                command = self.queue.get(block=False)
-                self.handle_command(command)
-                print('Command received %s' % command)
-            except Exception as e:
-                #print(e)
-                pass
-
-            #time.sleep(0.3)
-
-
-if __name__ == '__main__':
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    dialog = QtWidgets.QMainWindow()
-    dialog.show()
-
-    ui_queue = Queue()
-    io = txteditor.txteditor_GUI(dialog, ui_queue)
-
-    client = Client(io)
-
-    #srv_addr = '127.0.0.1'
-    #client.connect(srv_addr, 'Markus')
-
-
-    ui_listener_thread = listen_ui(ui_queue, client)
-
-    ui_listener_thread.start()
-
-    # TODO
-    sys.exit(app.exec_())
-
-    print ('Terminating')
-
-    
-=======
         self.network_loop()
->>>>>>> paleodieet
